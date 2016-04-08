d9b5115256e025c2f31b0d7e72605be3dc8a0a32
HongminWang <wanghongminwhm@gmail.com>
2016-04-08 15:54:28 +0800

    Moved test/ outside git

--- test/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Please give log file path as var1 and commit# as var2"
-for i in `ls official_submissions/*`; do echo $i; ./m2scorer $i example/conll14.m2 > $1/log`basename $i`_$2.out 2>$1/log`basename $i`_$2.err; done
